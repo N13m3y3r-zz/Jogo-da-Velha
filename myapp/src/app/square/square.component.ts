e5ac55dc31fb271b39f834231bbc3ec533ccbb8e
N13m3y3r <niemeyer.bruno@gmail.com>
2020-04-30 22:15:11 -0300

    Component

--- myapp/src/app/square/square.component.ts
+++ myapp/src/app/square/square.component.ts
@@ -1,16 +1,14 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      {{ rando }}
-    </p>
+    <button>{{value}}</button>
   `,
   styles: [],
 })
 export class SquareComponent  {
 
-  rando = Math.random();
+    @Input() value : 'X' | 'O' ;
 
 }
