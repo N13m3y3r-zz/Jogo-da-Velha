09783ce078e4f315fa0e9344a6e69e35981cdcad
N13m3y3r <niemeyer.bruno@gmail.com>
2020-04-30 22:00:52 -0300

    Update square.component.ts

--- myapp/src/app/square/square.component.ts
+++ myapp/src/app/square/square.component.ts
@@ -4,11 +4,13 @@ import { Component} from '@angular/core';
   selector: 'app-square',
   template: `
     <p>
-      square works!
+      {{ rando }}
     </p>
   `,
   styles: [],
 })
 export class SquareComponent  {
+
+  rando = Math.random();
 
 }
